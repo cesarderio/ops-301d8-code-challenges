9da3caeb3c36e2808c5e9084ca02dd36f1007f38
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-05-31 12:44:10 -0700

    setting up for code challenge 01

---
+++ Challenge01/Challenge01.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Copy & Append Date and Time
+# Author:                       Raphael Chookagian
+# Date of latest revision:      05/31/2023
+# Purpose:                      Create a bash script that:
+# Copies /var/log/syslog to the current working directory
+# Appends the current date and time to the filename
+
+
+
+# Declare Variables
+
+# Declare Functions
+
+# Create
+
+# Main
+
+# End

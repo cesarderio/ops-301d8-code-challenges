55cd80f2f2002de85ef4cc5e4a9dea889378e2d7
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-05-31 15:37:19 -0700

    working on code challenge 01

--- Challenge01/Challenge01.sh
+++ Challenge01/Challenge01.sh
@@ -8,8 +8,32 @@
 # Appends the current date and time to the filename
 
 
-
 # Declare Variables
+
+
+# current working directory
+curr_loki=$(pwd)
+
+# log/file to copy
+sys_log="/var/log/syslog"
+
+# where to copy log/file to
+# and append date and time to filename
+daTime=$(date +"%Y%m%d")
+logFile="$curr_loki/syslog_$daTime"
+
+# tell user process has started at date and time
+star_Time=$(date +"%Y_%m_%d %H:%M:%S")
+echo "[$star_Time] Cloning of system logs has begun"
+
+# copy log to current location
+cp "$sys_log" "$logFile"
+
+# let user know system log is being copied
+echo "[$star_Time] Cloning in process"
+
+
+
 
 # Declare Functions
 
---
+++ nmap/test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Script Name:                  GitHub
+# Author:                       Raphael Chookagian
+# Date of latest revision:      05/10/2023
+# Purpose:                      
+
+
+
+# Declare Variables
+
+# Declare Functions
+
+# Create
+
+# Main
+
+
+
+
+# host discovery
+# sudo nmap -sn <target IP range>
+# sudo nmap -sn 192.168.1.102 # <-- For Kali VM 05.31.2023
+
+# host discovery and output to file
+sudo nmap -sn 192.168.1.102 >> ~/Desktop/scan_Output.txt
+
+
+
+# Perform a fast, aggressive scan of the network.
+sudo nmap -T4 -A 192.168.1.102 >> ~/Desktop/scan_Output.txt
+
+# Scan the 1000 most common ports on each host on the network.
+sudo nmap -p- --top-ports 1000 192.168.1.102 >> ~/Desktop/scan_Output.txt
+
+# Perform an intense scan on all hosts on the network.
+sudo nmap -T4 -p- 192.168.1.102 >> ~/Desktop/scan_Output.txt
+
+
+# Perform a slow, comprehensive scan on all hosts on the network.
+sudo nmap -T2 -p- 192.168.1.102 >> ~/Desktop/scan_Output.txt
+
+
+
+# (Windows10) From a terminal prompt, run a netstat command that shows all active ports.
+netstat -ano # ano displays active connections and listening ports
+
+# on a specific IP or machine/target
+# netstat -ano -a -n <target IP/hostname>
+
+
+# Run a netstat command that displays the routing table.
+netstat -r # -r flag displays routing table
+
+# Run a netstat command that only displays connections for the TCP protocol.
+# -p tcp tells "netstat" to filter and display only TCP connections.
+netstat -ano -p tcp 
+
+
+
+
+
+# End

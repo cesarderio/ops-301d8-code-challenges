6efb98cd040ab9b7fbe41e1722090fd95fd2b863
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-05-31 16:37:12 -0700

    added scripts and commands for nmap for the lab 02 tasks

--- nmap/test.sh
+++ nmap/test.sh
@@ -3,20 +3,12 @@
 # Script Name:                  GitHub
 # Author:                       Raphael Chookagian
 # Date of latest revision:      05/10/2023
-# Purpose:                      
-
-
+# Purpose:                      scripts I put together for 301d8 lab 02 for nmap commands
 
 # Declare Variables
-
 # Declare Functions
-
 # Create
-
 # Main
-
-
-
 
 # host discovery
 # sudo nmap -sn <target IP range>
@@ -24,8 +16,6 @@
 
 # host discovery and output to file
 sudo nmap -sn 192.168.1.102 >> ~/Desktop/scan_Output.txt
-
-
 
 # Perform a fast, aggressive scan of the network.
 sudo nmap -T4 -A 192.168.1.102 >> ~/Desktop/scan_Output.txt
@@ -36,18 +26,14 @@ sudo nmap -p- --top-ports 1000 192.168.1.102 >> ~/Desktop/scan_Output.txt
 # Perform an intense scan on all hosts on the network.
 sudo nmap -T4 -p- 192.168.1.102 >> ~/Desktop/scan_Output.txt
 
-
 # Perform a slow, comprehensive scan on all hosts on the network.
 sudo nmap -T2 -p- 192.168.1.102 >> ~/Desktop/scan_Output.txt
-
-
 
 # (Windows10) From a terminal prompt, run a netstat command that shows all active ports.
 netstat -ano # ano displays active connections and listening ports
 
 # on a specific IP or machine/target
 # netstat -ano -a -n <target IP/hostname>
-
 
 # Run a netstat command that displays the routing table.
 netstat -r # -r flag displays routing table

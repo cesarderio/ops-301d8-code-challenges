b813c86a7f4e23ca2b6db3a6b71f27a17894347a
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-06-01 18:00:58 -0700

    working on code challenge 2

---
+++ Challenge02/Challenge02.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Script Name:                  Copy & Append Date and Time
+# Author:                       Raphael Chookagian
+# Date of latest revision:      05/31/2023
+# Purpose:                      Create a bash script that:
+
+# Prompts user for input directory path.
+# Prompts user for input permissions number (e.g. **777** to perform a **chmod 777**)
+# Navigates to the directory input by the user and changes all files inside it to the input setting.
+# Prints to the screen the directory contents and the new permissions settings of everything in the directory.
+
+# Main
+
+# Prompts user for input directory path.
+read -p "Enter your target directory path: " directory
+
+# Check if directory exists
+if [ -d "$directory" ]; then
+    
+# Prompts user for input permissions number (e.g. **777** to perform a **chmod 777**)
+    read -p "Enter permissions number (e.g. 777): " permissions
+    
+    # Check for valid input
+    if [[ $permissions =~ ^[0-7]{3}$ ]]; then
+
+# Navigates to the directory input by the user and changes all files inside it to the input setting.
+        for file in "$directory"/*; do
+            if [ -f "$file" ]; then
+                # execute chmod command
+                chmod "$permissions" "$file"
+                echo "Permissions updated: $file"
+            fi
+        done
+        
+# Prints to the screen the directory contents and the new permissions settings of everything in the directory.
+        echo "Updated directory settings:"
+        ls -l "$directory"
+    else
+        echo "Invalid input, Enter a valid 3-digit number."
+    fi
+else
+    echo "'$directory' does not exist."
+fi
+
+# End
---
+++ Challenge02/README.md
@@ -0,0 +1,27 @@
+# Ops Challenge: File Permissions
+
+## Overview
+
+Linux sets file permissions by User, Group, and Other. Linux also sorts permission by Read, Write, and Execute. Today, you will be create a bash script that alters file permissions of everything in a directory.
+
+## Objectives
+
+Take care to only perform this operation in user-created directories. Changing permissions in system files/directories is not advised, as this can cause malfunctions in the OS.
+
+### Create a new bash script that performs the following
+
+* Prompts user for input directory path.
+
+* Prompts user for input permissions number (e.g. **777** to perform a **chmod 777**)
+
+* Navigates to the directory input by the user and changes all files inside it to the input setting.
+
+* Prints to the screen the directory contents and the new permissions settings of everything in the directory.
+
+## Stretch Goals (Optional Objectives)
+
+* Pursue stretch goals if you are a more advanced user or have remaining lab time.
+
+* Design your script to output a log file of all actions that were taken by the script.
+
+* Design your script to output to the screen each file changed one by one, with a slight delay between each file changed.

5952d8c51f407f2f2bbcbc76e3c948c35bb78939
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-06-08 22:08:11 -0700

    working on stretch goals on code challenge 07 and using chatGPT to make it extra

--- Challenge07/Challenge07.py
+++ Challenge07/Challenge07.py
@@ -1,75 +1,169 @@
 #!/usr/bin/env python3
 
-# Script Name:                  Bash in Python
+# Script Name:                  Python
 # Author:                       Raphael Chookagian
-# Date of latest revision:      06/07/2023
+# Date of latest revision:      06/08/2023
 # Purpose:                      Create a Python script that includes the following:
           # - Assign to a variable a list of ten string elements.
           # - Print the fourth element of the list.
           # - Print the sixth through tenth element of the list.
           # - Change the value of the seventh element to "onion".
 
+import os
+
+# pre-check (for stretch goal)
+# Delete the file if it already exists
+file_path = "linux_distros_list.txt"
+if os.path.isfile(file_path):
+    os.remove(file_path)
+
 # Declaration of variables
+
 # - Assign to a variable a list of ten string elements.
-Linux_Distros = ['Arch', 'Debian','Elementary', 'Fedora', 'Kali', 'Mint', 'Parrot','Pop', 'Ubuntu', 'Zorin']
+linux_distros = ['Arch', 'Debian','Elementary', 'Fedora', 'Kali', 'Mint', 'Parrot','Pop', 'Ubuntu', 'Zorin']
 
 # Main
+
 print()
 # - Print the fourth element of the list.
-print("Number four on the list of Linux Distributions:", Linux_Distros[3], "OS")
+print("Number four on the list of Linux Distributions: ", linux_distros[3], "OS")
 print()
 # - Print the sixth through tenth element of the list.
-print("Numbers six to ten on the list of Linux Distributions:")
-for distro in Linux_Distros[5:10]:
-    print(distro + "OS")
+print("Numbers six to ten on the list of Linux Distributions: ")
+for distro in linux_distros[5:10]:
+    print(distro + " OS")
 
 # - Change the value of the seventh element to "onion".
 # Linux_Distros[5] = "MX"
-Linux_Distros[6] = "onion"
+linux_distros[6] = "onion"
 
-# print("Updated list of Linux Distributions:", Linux_Distros)
-
+# print("Updated list of Linux Distributions:")
+# for distro in Linux_Distros:
+#     print(distro + "OS")
 
 
 # Stretch goals
+# I have my basic stretch goals commented out for each below. 
+# The extra user prompts and things were done with the help of chatGPT
 
 # append
-Linux_Distros.append("CentOS")
+# Linux_Distros.append("CentOS")
+user_input = input("Would you like to add a distribution? (y/n): ")
+if user_input.lower() == "y":
+    distro_name = input("Enter the name of the distribution you would like to add to the list: ")
+    linux_distros.append(distro_name)
+
 
 # clear the list
 # Linux_Distros.clear()
+user_input = input("Would you like to clear the list? (y/n): ")
+if user_input.lower() == "y":
+    linux_distros.clear()
+    print("List cleared.")
 
-# copy the list
-distros_copy = Linux_Distros.copy()
 
 # count occurences of element
-count_element = Linux_Distros.count("Ubuntu")
+# count_element = Linux_Distros.count("Ubuntu")
+user_input = input("Would you like to count the occurrences of a distribution on this list? (y/n): ")
+if user_input.lower() == "y":
+    distro_name = input("Enter the name of the distribution: ")
+    count_distro = linux_distros.count(distro_name)
+    print(f"The distribution '{distro_name}' appears {count_distro} times in this list.")
+
 
 # extend/add list
 # extra_distros = ["Gentoo", "Red Hat"]
 # Linux_Distros.extend(extra_distros)
+user_input = input("Would you like to add to the list? (y/n): ")
+if user_input.lower() == "y":
+    additional_distros = input("Enter the names of the distributions to add (comma-separated): ")
+    distro_list = additional_distros.split(",")
+    linux_distros.extend(distro_list)
 
 # index of element
-index_element = Linux_Distros.index("Mint")
+# index_element = linux_distros.index("Mint")
+# index of element
+user_input = input("Do you want to find the index of a distribution? (y/n): ")
+if user_input.lower() == "y":
+    distro_name = input("Enter the name of the distribution to find the index for: ")
+    try:
+        index_element = linux_distros.index(distro_name)
+        print(f"The distribution '{distro_name}' is found at index {index_element}.")
+    except ValueError:
+        print(f"The distribution '{distro_name}' was not found.")
 
 # insert at index
-Linux_Distros.insert(3, "Cent")
+# linux_distros.insert(3, "Cent")
+user_input = input("Do you want to insert a distribution at a specific index? (y/n): ")
+if user_input.lower() == "y":
+    distro_name = input("Enter the name of the distribution to insert: ")
+    index = int(input("Enter the index to insert the distribution at: "))
+    linux_distros.insert(index, distro_name)
+
 
 # - Remove an element from the list
-Linux_Distros.remove("Fedora")
+# linux_distros.remove("Fedora")
+# remove an element from the list
+user_input = input("Do you want to remove a distribution from the list? (y/n): ")
+if user_input.lower() == "y":
+    distro_name = input("Enter the name of the distribution to remove: ")
+    if distro_name in Linux_Distros:
+        Linux_Distros.remove(distro_name)
+        print(f"The distribution '{distro_name}' has been removed from the list.")
+    else:
+        print(f"The distribution '{distro_name}' is not in the list.")
+
 
 # - Remove and return the last element from the list
-last_element = Linux_Distros.pop()
+last_element = linux_distros.pop()
 
 # - Reverse the order of the list
-Linux_Distros.reverse()
+linux_distros.reverse()
 
 # - Sort the list in ascending order
-Linux_Distros.sort()
+linux_distros.sort()
 
 print()
 print("Updated list of Linux Distributions:")
-for distro in Linux_Distros:
+for distro in linux_distros:
     print(distro + "OS")
 
+
+
+# copy the list
+# distros_copy = linux_distros.copy()
+user_input = input("Would you like to make a copy of this list? (y/n): ")
+if user_input.lower() == "y":
+    with open(file_path, "w") as file:
+        for distro in linux_distros:
+            file.write(distro + " OS\n")
+    print(f"List copied to '{file_path}'.")
+else:
+    print("List not saved")
+
 # End
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

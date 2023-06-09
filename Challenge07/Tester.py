a93e5968c39c75c98153005320ead5acd8aa79c4
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-06-08 23:03:18 -0700

    working on stretch goals on code challenge 7 tester

--- Challenge07/Tester.py
+++ Challenge07/Tester.py
@@ -176,73 +176,76 @@ for distro in linux_distros:
 
 
 print()
-
 commands = [
     "append",
+    "ascending",
     "clear",
+    "copy",
     "count",
     "extend",
     "index",
     "insert",
-    "remove",
     "last",
+    "quit",
+    "remove",
     "reverse",
-    "ascending",
-    "copy"
 ]
+
+commands.sort()  # Sort the commands in alphabetical order
 
 print("Select a command:")
 for i, command in enumerate(commands):
     print(f"{i + 1}. {command}")
 
-user_input = input("Enter the number of the command you want to execute: ")
-command_index = int(user_input) - 1
+while True:
+    user_input = input("Enter the number of the command you want to execute (or 'q' to quit): ")
+    if user_input.lower() == "q":
+        break
+    command_index = int(user_input) - 1
 
-if command_index >= 0 and command_index < len(commands):
-    command = commands[command_index]
-    if command == "append":
-        distro_name = input("Enter the name of the distribution you would like to add to the list: ")
-        linux_distros.append(distro_name)
-    elif command == "clear":
-        linux_distros.clear()
-        print("List cleared.")
-    elif command == "count":
-        distro_name = input("Enter the name of the distribution: ")
-        count_distro = linux_distros.count(distro_name)
-        print(f"The distribution '{distro_name}' appears {count_distro} times in this list.")
-    elif command == "extend":
-        additional_distros = input("Enter the names of the distributions to add (comma-separated): ")
-        distro_list = additional_distros.split(",")
-        linux_distros.extend(distro_list)
-    elif command == "index":
-        distro_name = input("Enter the name of the distribution to find the index for: ")
-        try:
-            index_element = linux_distros.index(distro_name)
-            print(f"The distribution '{distro_name}' is found at index {index_element}.")
-        except ValueError:
-            print(f"The distribution '{distro_name}' was not found.")
-    elif command == "insert":
-        distro_name = input("Enter the name of the distribution to insert: ")
-        index = int(input("Enter the index to insert the distribution at: "))
-        linux_distros.insert(index, distro_name)
-    elif command == "remove":
-        distro_name = input("Enter the name of the distribution to remove: ")
-        if distro_name in linux_distros:
-            linux_distros.remove(distro_name)
-            print(f"The distribution '{distro_name}' has been removed from the list.")
-        else:
-            print(f"The distribution '{distro_name}' is not in the list.")
-    elif command == "last":
-        user_input = input("Do you want to remove the last element? (y/n): ")
-        if user_input.lower() == "y":
+    if command_index >= 0 and command_index < len(commands):
+        command = commands[command_index]
+        if command == "append":
+            distro_name = input("Enter the name of the distribution you would like to add to the list: ")
+            linux_distros.append(distro_name)
+        elif command == "ascending":
+            linux_distros.sort()
+            print("The list has been sorted in ascending order.")
+        elif command == "clear":
+            linux_distros.clear()
+            print("List cleared.")
+        elif command == "copy":
+            linux_distros_copy = linux_distros.copy()
+            print("A copy of the list has been created.")
+        elif command == "count":
+            distro_name = input("Enter the name of the distribution: ")
+            count_distro = linux_distros.count(distro_name)
+            print(f"The distribution '{distro_name}' appears {count_distro} times in this list.")
+        elif command == "extend":
+            additional_distros = input("Enter the names of the distributions to add (comma-separated): ")
+            distro_list = additional_distros.split(",")
+            linux_distros.extend(distro_list)
+        elif command == "index":
+            distro_name = input("Enter the name of the distribution to find the index for: ")
+            try:
+                index_element = linux_distros.index(distro_name)
+                print(f"The distribution '{distro_name}' is found at index {index_element}.")
+            except ValueError:
+                print(f"The distribution '{distro_name}' was not found.")
+        elif command == "insert":
+            distro_name = input("Enter the name of the distribution to insert: ")
+            index = int(input("Enter the index to insert the distribution at: "))
+            linux_distros.insert(index, distro_name)
+        elif command == "last":
             last_element = linux_distros.pop()
             print(f"The last element '{last_element}' has been removed from the list.")
-    elif command == "reverse":
-        linux_distros.reverse()
-        print("The list has been reversed.")
-    elif command == "ascending":
-        linux_distros.sort()
-        print("The list has been sorted in ascending order.")
-    elif command == "copy":
-        linux_distros_copy = linux_distros.copy()
-        print("A copy of the list has been created.")
+        elif command == "remove":
+            distro_name = input("Enter the name of the distribution to remove: ")
+            if distro_name in linux_distros:
+                linux_distros.remove(distro_name)
+                print(f"The distribution '{distro_name}' has been removed from the list.")
+            else:
+                print(f"The distribution '{distro_name}' is not in the list.")
+        elif command == "reverse":
+            linux_distros.reverse()
+            print("The list has been reversed.")
